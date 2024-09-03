a02836d26a782a9e2aa72998e97aacdaa58c7b19
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2024-09-03 04:33:44 -0300

    melhorado questao 5

--- questao5/main.js
+++ questao5/main.js
@@ -1,10 +1,16 @@
-const str = "abba"
-const size = str.length
+const strings = ["abcdefg", "bcdfgdeeasdw", "aaabbb"];
+var stringsReverse = [] 
+console.log(`Strings originais:\n${strings.join(", ")}\n`);
 
-let reversed = []
+strings.forEach((str) => {
+  const size = str.length
+  let reversed = [];
 
-for (var i = 0; i < size; i = i + 1){
-  reversed[i] = str[size - i - 1]
-}
+  for (var i = 0; i < size; i = i + 1) {
+    reversed[i] = str[size - i - 1];
+  }
 
-console.log(reversed.join(''))
+  stringsReverse.push(reversed.join(""));
+});
+
+console.log(`Strings invertidas:\n${stringsReverse.join(", ")}`)
